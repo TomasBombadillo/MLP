770c18775e2fb58c94124d596bf1b63f558f4d86
TomasBombadillo <42761681+TomasBombadillo@users.noreply.github.com>
2018-08-31 00:55:50 -0500

    Trained version
    Now it is trained, but only with one data, it shows the error graph and commented if the Xavier init, sometimes work, but not always, while only random weights always work.

--- MLPlib.py
+++ MLPlib.py
@@ -2,21 +2,11 @@
 #----------- Multi-Layer Perceptron -----------------
 #####################################################
 #------------Author: Tomas Londono------------------- 
-############# Last edit: 22/8/18 ####################
+############# Last edit: 31/8/18 ####################
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-def ReLU(x):
-	if x <0 :
-		return 0
-	elif x >=0:
-		return x
-
-def dReLU(x):
-	if x<0:
-		return 0
-	elif x<=0:
-		return 1
 
 class perceptron(object):
 	'''
@@ -24,8 +14,9 @@ class perceptron(object):
 	it ask the user the number of layers, the number of entries and
 	the number of neurons per layer.
 	'''
-	def __init__(self, layers, activ_function):
+	def __init__(self, layers, activ_function,devActiv_function):
 		self.f = activ_function
+		self.df = devActiv_function
 
 		# Number of layers
 		self.n_layers = len(layers)-1
@@ -42,9 +33,16 @@ class perceptron(object):
 		Delta = [np.array([])]
 
 		for i in range(1, self.n_layers+1):
+			# Xavier initialization
+			#------------------------
+			# usually work, but sometimes stay in error=1 and doesn't move anywhere
+
+			#sigma = np.sqrt(2./(self.layer[i-1]+self.layer[i]))
+			#W.append( np.array(np.random.normal(0,sigma,(self.layer[i-1],self.layer[i]))) )
+
 			# Create random matrices with dimensions specified
-			W.append( np.array([np.random.random( (self.layer[i-1],self.layer[i]) )]) )
-			B.append( np.array([np.random.random( self.layer[i] )]) )
+			W.append( np.array(np.random.random( (self.layer[i-1],self.layer[i]) )) )
+			B.append( np.zeros((1,self.layer[i])) )
 
 			delta.append( np.zeros((1,self.layer[i])) )
 			Delta.append( np.zeros((1,self.layer[i])) )
@@ -63,7 +61,7 @@ class perceptron(object):
 			self.X = []
 			self.Z = [[]]
 
-		self.X.append( X )
+		self.X.append( np.array([X]) )
 		for i in range(1,self.n_layers+1):
 			# Evaluates the result of the past layer ands send it to the next one
 			Z = np.array([(np.dot(X,self.W[i]) + self.B[i]).reshape(-1)])
@@ -72,45 +70,48 @@ class perceptron(object):
 			#saves values for back-propagation
 			self.Z.append( Z )
 			self.X.append( X )
+
 		return X
 
-	def Train(self, X, Y):
-		J = np.sum(self.Work(X)-Y)**2 /len(Y)
-		'''
-		try:
-			self.delta
-			self.Delta
-		except AttributeError:
-			for i in range(1, self.n_layers+1):
-				self.delta.append( np.zeros((self.layer[i-1, self.layer[i]])) )
-				self.Delta.append( np.zeros((self.layer[i-1, self.layer[i]])) )
-		'''
+	def Train(self, X, Y, learn_rate=0.001, epochs=10000):
+		errors = np.array([])
+
+		Y_pred = self.Work(X)
+		J = np.sum(Y_pred-Y)**2 /len(Y)
+		errors = np.append(errors,J)
+
 		epsilon = 1e-5
 		i=0
-		while J > epsilon or i>100:
+		while J > epsilon and i<epochs:
 			i+=1
-			for j in range(self.n_layers+1):
+			self.delta[-1] = (self.X[-1]-Y)/ len(Y)
+			for j in range(self.n_layers):
 				l = self.n_layers-j
 
-				if l==self.n_layers: delta = (self.X[l]-Y)/ len(X)
-				Delta = delta * self.X[l].T
+				self.Delta[l] = self.delta[l] * np.vectorize(self.df)(self.Z[l])
+				self.delta[l-1] = np.dot(self.Delta[l],self.W[l].T)
 
-				self.delta.append(delta)
-				self.Delta.append(Delta)
+			for j in range(self.n_layers):
+				l = self.n_layers-j
 
-				# PRINT
-				#print(l,self.X[l], self.delta[l], self.Delta[l])
-				#print(l, delta,delta.shape,Delta,Delta.shape)
-			if i==1: break
+				gradW = np.dot(self.X[l-1].T,self.Delta[l])
+				gradB = np.sum(self.Delta[l], axis=0).reshape((1,-1))
 
+				self.W[l] -= learn_rate*gradW
+				self.B[l] -= learn_rate*gradB
 
+			if i%100==0:
+				Y_pred = self.Work(X)
+				J = np.sum(Y_pred-Y)**2 /len(Y)
+				print("iter: ",i,"Value:\t",Y_pred,"Error:\t",J*100)
+				errors = np.append(errors,J)
 
-			
+		Y_pred = self.Work(X)
+		J = np.sum(Y_pred-Y)**2 /len(Y)
+		print("Value:\t",Y_pred,"Error:\t",J*100)
 
-		pass
-
-	def BackPropagation(self, df):
-		pass
-
-
+		plt.plot(np.linspace(0,len(errors)*100,len(errors)),errors)
+		plt.show()
+		plt.close()
+		return J
 
--- exec.py
+++ exec.py
@@ -1,26 +1,31 @@
 from MLPlib import *
 import numpy as np
 
+def ReLU(x):
+	if x <0 :
+		return 0
+	elif x >=0:
+		return x
+
+def dReLU(x):
+	if x<0:
+		return 0
+	elif x>=0:
+		return 1
 
 neu_per_layer = np.array([2,2,1])
 # Create network with ReLU activation function
-P = perceptron(neu_per_layer, ReLU)
+P = perceptron(neu_per_layer, ReLU, dReLU)
 
 # ask for Input data
 # X = np.array(list(map(float, input("Ingresar datos de entrada:\t").split(" ")))).reshape([1,-1])
-X = np.array([[1,1],[-1,1],[1,-1],[-1,-1]]) #XOR DATA
+X = np.array([[0,0],[0,1],[1,0],[1,1]]) #XOR DATA
 Y = np.array([[0],[1],[1],[0]])
 
 # Shows the result to the user
-print(P.Work(X[0]))
-
-# Prints
-'''
-print(P.n_layers)
-for Xl in P.X:
-	print(Xl)
-print(P.X[1])
-'''
+print("Initial value:\t",P.Work(X[1]))
 
 # Training
-print(P.Train(X[0],Y[0]))
+P.Train(X[1],Y[1])
+
+print("Final value:\t",P.Work(X[1]))
