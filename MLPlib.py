3849f9a6708297794699a474ac22e37b3fce9b02
TomasBombadillo <42761681+TomasBombadillo@users.noreply.github.com>
2018-09-05 01:39:31 -0500

    Working more than 50% of times
    using "Hay un artículo del 1991 por Hirose, Yamashita y Huiya que explica que hay 16 mínimos locales a los que puede converger la red. Para mejorar la convergencia hay que usar función de activación Sigmoid en ambas capas, inicializar los pesos de la red con una distribución uniforme entre -1 y 1, usar gradiente descendente con una tasa de aprendizaje alta (pueden tomar un valor de 2) y correrlo por mínimo 5000 épocas".
    I used learn_rate=2, swish in firstlayer and sigmoid second layer, schedule potential learning rate to converge faster.

--- MLPlib.py
+++ MLPlib.py
@@ -36,13 +36,14 @@ class perceptron(object):
 			# Xavier initialization
 			#------------------------
 			# usually work, but sometimes stay in error=1 and doesn't move anywhere
-
-			#sigma = np.sqrt(2./(self.layer[i-1]+self.layer[i]))
-			#W.append( np.array(np.random.normal(0,sigma,(self.layer[i-1],self.layer[i]))) )
+			sigma = np.sqrt(2./(self.layer[i-1]+self.layer[i]))
+			W.append( np.array(np.random.normal(0.5,sigma,(self.layer[i-1],self.layer[i]))) )
+			#B.append( np.zeros((1,self.layer[i])) )
 
 			# Create random matrices with dimensions specified
-			W.append( np.array(np.random.random( (self.layer[i-1],self.layer[i]) )) )
-			B.append( np.zeros((1,self.layer[i])) )
+			#W.append( np.random.uniform( -1,1, size=(self.layer[i-1],self.layer[i]) ) )
+			B.append( np.array(np.random.uniform( size=(1,self.layer[i]) )) )
+
 
 			delta.append( np.zeros((1,self.layer[i])) )
 			Delta.append( np.zeros((1,self.layer[i])) )
@@ -53,6 +54,9 @@ class perceptron(object):
 		self.delta = delta
 		self.Delta = Delta
 
+	def linear_J(self,Y_pred,Y):
+		return np.sum((Y-Y_pred)*(Y-Y_pred)) / len(Y)
+
 	def Work(self, X): # evaluates the entries through all network and return result
 		try:
 			self.X
@@ -61,11 +65,11 @@ class perceptron(object):
 			self.X = []
 			self.Z = [[]]
 
-		self.X.append( np.array([X]) )
+		self.X.append( np.array(X) )
 		for i in range(1,self.n_layers+1):
 			# Evaluates the result of the past layer ands send it to the next one
-			Z = np.array([(np.dot(X,self.W[i]) + self.B[i]).reshape(-1)])
-			X = np.vectorize(self.f)( Z )
+			Z = np.array([(np.dot(X,self.W[i]) + self.B[i]).reshape(-1)]).reshape( (X.shape[0],self.B[i].shape[1]) )
+			X = np.vectorize(self.f[i-1])(Z )
 
 			#saves values for back-propagation
 			self.Z.append( Z )
@@ -73,45 +77,57 @@ class perceptron(object):
 
 		return X
 
-	def Train(self, X, Y, learn_rate=0.001, epochs=10000):
+	def Train(self, X, Y, learn_rate=0.01, epochs=10000):
 		errors = np.array([])
 
 		Y_pred = self.Work(X)
-		J = np.sum(Y_pred-Y)**2 /len(Y)
+		J = self.linear_J(Y_pred,Y)
+		print("Initial Error: \t",J*100) 
 		errors = np.append(errors,J)
 
-		epsilon = 1e-5
+		epsilon = 1e-10
+		step=epochs/1000.
+		umbral=1000
+
 		i=0
 		while J > epsilon and i<epochs:
-			i+=1
-			self.delta[-1] = (self.X[-1]-Y)/ len(Y)
+			Y_pred = self.Work(X)
+			J = self.linear_J(Y_pred,Y)
+
+			self.delta[-1] = (self.X[-1]-Y)*2./ len(Y)
+
 			for j in range(self.n_layers):
 				l = self.n_layers-j
-
-				self.Delta[l] = self.delta[l] * np.vectorize(self.df)(self.Z[l])
+				self.Delta[l] = self.delta[l] * np.vectorize(self.df[l-1])(self.Z[l])
 				self.delta[l-1] = np.dot(self.Delta[l],self.W[l].T)
 
 			for j in range(self.n_layers):
 				l = self.n_layers-j
-
+				
 				gradW = np.dot(self.X[l-1].T,self.Delta[l])
 				gradB = np.sum(self.Delta[l], axis=0).reshape((1,-1))
-
+				
 				self.W[l] -= learn_rate*gradW
-				self.B[l] -= learn_rate*gradB
+				self.B[l] -= learn_rate*gradB			
 
-			if i%100==0:
+			if i==1000:
+				learn_rate0 = learn_rate
+			elif i>1000:
+				learn_rate = learn_rate0*i/umbral
+
+			if i%step==0:	
 				Y_pred = self.Work(X)
-				J = np.sum(Y_pred-Y)**2 /len(Y)
-				print("iter: ",i,"Value:\t",Y_pred,"Error:\t",J*100)
-				errors = np.append(errors,J)
+				J = self.linear_J(Y_pred,Y)
+				print("\nEpoch: ",i," learn_rate: ",learn_rate,"\tError:\t",J*100,"%")
+			
+			errors = np.append(errors,J)
 
-		Y_pred = self.Work(X)
-		J = np.sum(Y_pred-Y)**2 /len(Y)
-		print("Value:\t",Y_pred,"Error:\t",J*100)
+			i+=1 
 
-		plt.plot(np.linspace(0,len(errors)*100,len(errors)),errors)
+		plt.plot(np.linspace(0,len(errors),len(errors)),errors)
 		plt.show()
 		plt.close()
+
+	
 		return J
 
--- exec.py
+++ exec.py
@@ -6,26 +6,71 @@ def ReLU(x):
 		return 0
 	elif x >=0:
 		return x
-
 def dReLU(x):
 	if x<0:
 		return 0
 	elif x>=0:
 		return 1
 
-neu_per_layer = np.array([2,2,1])
-# Create network with ReLU activation function
-P = perceptron(neu_per_layer, ReLU, dReLU)
+def sigmoid(x):
+    "Numerically stable sigmoid function."
+    if x >= 0:
+        z = np.exp(-x)
+        return 1 / (1 + z)
+    else:
+        # if x is less than zero then z will be small, denom can't be
+        # zero because it's 1+z.
+        z = np.exp(x)
+        return z / (1 + z)
+def dsigmoid(x):
+	return sigmoid(x)*(1-sigmoid(x))
 
-# ask for Input data
-# X = np.array(list(map(float, input("Ingresar datos de entrada:\t").split(" ")))).reshape([1,-1])
+def swish(x):
+	return x*sigmoid(x)
+def dswish(x):
+	return sigmoid(x)+x*dsigmoid(x)
+
+def SELU(x):
+	lamb=1.
+	alpha=10.
+	if x <=0 :
+		return lamb*alpha*(np.exp(x)-1.)
+	elif x >0:
+		return lamb*x
+
+def dSELU(x):
+	lamb=1.
+	alpha=10.
+	if x <=0 :
+		return lamb*alpha*(np.exp(x))
+	elif x >0:
+		return lamb
+
+def identity(x):
+	return x 
+def didentity(x):
+	return 1
+
+# Array of numberof neurons per layer
+neu_per_layer = np.array([2,2,1])
+
+# Array of functions
+f = [swish, sigmoid]
+df = [dswish, dsigmoid]
+
+# Create network with ReLU activation function
+P = perceptron(neu_per_layer, f, df)
+
+# Input data
 X = np.array([[0,0],[0,1],[1,0],[1,1]]) #XOR DATA
+
+# Correct output values
 Y = np.array([[0],[1],[1],[0]])
 
 # Shows the result to the user
-print("Initial value:\t",P.Work(X[1]))
+print("Initial value:\n",P.Work(X))
 
 # Training
-P.Train(X[1],Y[1])
+P.Train(X,Y,learn_rate = 2.,epochs=100000)
 
-print("Final value:\t",P.Work(X[1]))
+print("Final value:\n",P.Work(X))
